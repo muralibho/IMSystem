68f075983a4202ab339d708938238aa854e63956
muralibmur <murali.ang2018@gmail.com>
2022-11-16 22:03:32 +0530

    Project Architecture Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { ServicesComponent } from './services/services.component';
+import { GuardsComponent } from './guards/guards.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    ServicesComponent,
+    GuardsComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule,
