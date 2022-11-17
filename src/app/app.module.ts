1fb325b620d8d77eefdfd35f92dcb102889e9932
muralibmur <murali.ang2018@gmail.com>
2022-11-17 11:15:00 +0530

    Login Service Implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
@@ -26,6 +26,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ReactiveFormsModule,
     FormsModule
   ],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -11,13 +12,23 @@ export class LoginComponent implements OnInit {
   public email    : string = "";
   public password : string = "";
 
-  public loginForm: FormGroup = new FormGroup(
+  public loginForm:FormGroup = new FormGroup(
     {
       email: new FormControl(null, [Validators.required, Validators.minLength(15), Validators.maxLength(20)]),
       password: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(15)])
     }
   )
-  constructor() { }
+  constructor(private _loginService:LoginService) { 
+    this._loginService.getLoginForm().subscribe(
+      (data:any)=>{
+           this.loginForm = data;
+      },
+      (err:any)=>{
+         alert("Internal server error");
+      }
+
+    )
+  }
 
   ngOnInit(): void {
   }
